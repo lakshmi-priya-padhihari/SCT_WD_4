e9ebee74526ca18dcb2071077590f1098534835f
lakshmi-priya-padhihari <lakshmipriyapadhihari@gmail.com>
2024-09-15 19:19:19 +0530

    Add files via upload

---
+++ to do web app/index.js
@@ -0,0 +1,27 @@
+
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+    
+    if (taskInput.value === "") {
+        alert("Please enter a task!");
+        return;
+    }
+    
+   
+    const li = document.createElement('li');
+    li.textContent = taskInput.value;
+    
+  
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+    deleteBtn.onclick = function() {
+        taskList.removeChild(li);
+    };
+    
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    
+  
+    taskInput.value = "";
+}
